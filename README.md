58fc78e822653db1015e0a8b1b54858869a18cb3
dongkaiqi <87631658@qq.com>
2025-08-29 16:42:12 +0800

    feat: 🚀 init project

---
+++ README.md
@@ -0,0 +1,18 @@
+.
+├── apps
+│   ├── backend
+│   └── frontend
+├── demos
+│   ├── react
+│   ├── vanilla
+│   └── vue
+├── package.json
+├── packages
+│   ├── browser
+│   ├── browser-utils
+│   ├── core
+│   ├── react
+│   ├── utils
+│   └── vue
+├── README.md
+└── pnpm-workspace.yaml
---
+++ eslint.config.mjs
@@ -0,0 +1,92 @@
+import eslint from '@eslint/js'
+import globals from 'globals' // 需要处理全局变量：window console等
+import tseslint from 'typescript-eslint' // 需要对ts进行支持
+import eslintPrettier from 'eslint-plugin-prettier'
+import eslintPluginVue from 'eslint-plugin-vue'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+// 前端配置
+const frontendConfig = {
+  files: ['apps/frontend/monitor/**/*.{js,ts,vue,jsx,tsx}'], // 检查范围
+  ignores: ['apps/frontend/monitor/src/components/ui/**/*'], // 忽略检查
+  extends: [...eslintPluginVue.configs['flat/recommended']], // 继承配置
+  languageOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    globals: {
+      // 会自动引入前端相关的全局变量
+      ...globals.browser
+    },
+    parserOptions: {
+      // 针对react
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
+    parser: tseslint.parser
+  },
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      {
+        allowConstantExport: true
+      }
+    ]
+  }
+}
+
+// 后端配置
+const backendConfig = {
+  files: ['apps/backend/**/*.{js,ts}'],
+  languageOptions: {
+    globals: {
+      ...globals.node
+    },
+    parser: tseslint.parser
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    'no-undef': 'error'
+  }
+}
+
+const ignores = [
+  'dist',
+  'node_modules',
+  'build',
+  '**/*.js',
+  '**/*.mjs',
+  '**/*.d.ts',
+  'eslint.config.mjs',
+  'apps/frontend/monitor/src/components/ui/**/*'
+]
+
+export default tseslint.config(
+  // 通用配置
+  {
+    ignores,
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended], // 继承一些配置
+    plugins: {
+      prettier: eslintPrettier
+      // "simple-import-sort": importSort
+    },
+    // 自定义规则
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off'
+      // "simple-import-sort/imports": "error",
+      // "simple-import-sort/exports": "error"
+    }
+  },
+  // 前端配置
+  frontendConfig,
+  // 后端配置
+  backendConfig
+)
